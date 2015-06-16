de17cd24c2964b5a40e002e14b2cbeb5280202f2
Guilherme Teodoro <gui.teo2@gmail.com>
2015-06-16 02:36:18 -0300

    Actions de Maps

---
+++ app/controllers/maps_controller.rb
@@ -0,0 +1,54 @@
+class MapsController < ApplicationController
+  respond_to :json
+
+  def index
+    render json: { maps: Map.all }
+  end
+
+  def show
+    map = Map.find_by_name(params[:id])
+
+    render json: { name: map.name, routes: map.routes }
+  end
+
+  def create
+    map = Map.new(name: params[:name])
+    map.routes = load_routes_from_params
+
+    if map.save
+      render json: { message: 'Map created successfuly' }
+    else
+      render json: { error: map.errors.full_messages }
+    end
+  end
+
+  def update
+    map = Map.find_by_name(params[:id])
+    map.routes = load_routes_from_params
+
+    if map.save
+      render json: { message: 'Map updated successfuly' }
+    else
+      render json: { error: map.errors.full_messages }
+    end
+  end
+
+  def destroy
+    map = Map.find_by_name(params[:id])
+
+    if map.destroy
+      render json: { message: 'Map destroyed successfuly' }
+    else
+      render json: { error: map.errors.full_messages }
+    end
+  end
+
+  private
+  def load_routes_from_params
+    return unless params[:routes]
+
+    params[:routes].map do |route|
+      Route.new(origin: route[:origin], destination: route[:destination], distance: route[:distance])
+    end
+  end
+end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,14 +1,6 @@
 class Route < ActiveRecord::Base
   belongs_to :map
 
-  validates_presence_of :distance, :origin, :destination
-  validates :distance, numericality: { greater_than: 0 }
-  validate :destination_is_different_from_origin
-
-  private
-  def destination_is_different_from_origin
-    if (origin == destination) || (origin.name == destination.name)
-      errors.add(:destination, 'can\'t be equal to origin')
-    end
-  end
+  validates_presence_of :distance, :origin, :destination, :map
+  validates :distance, numericality: { greater_than_or_equal: 1 }
 end
